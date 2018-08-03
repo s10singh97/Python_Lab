1627690e2818564fb2f9340b9d76bfaea1fdddfd
s10singh97 <s10singh97@yahoo.in>
2018-08-03 14:54:40 +0530

    testing

---
+++ aa.py
@@ -0,0 +1,16 @@
+a = [1,2,3,4]
+i = 2; j = 4
+if i and j in a:
+    print('Present')
+else:
+    print('Not Present')
+    
+print(2+2)
+print(4^2)
+print(~4)
+print(4<<2)
+print(4>>2)
+print(2<3)
+print(2>4)
+print(2 == 2)
+print(2<=4)
