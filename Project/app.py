9a3f45ae0aa84321618a7db09859ba76ee51201f
s10singh97 <s10singh97@yahoo.in>
2018-10-30 23:59:48 +0530

    added login and logout methods along with session management

--- Project/app.py
+++ Project/app.py
@@ -1,11 +1,32 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, send_file
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
 
 import helpers
 from analyzer import Analyzer
 
+# configure application
 app = Flask(__name__)
 
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
@@ -39,3 +60,51 @@ def search():
 
     # render results
     return render_template("search.html", chart=chart, screen_name=screen_name)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Must provide username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Must provide password")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM users \
+                           WHERE username = :username", \
+                           username=request.form.get("username"))
+
+        # ensure username exists and password is correct
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("invalid username and/or password")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        flash("Login Successful!!", category="success")
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
